7b794be3b923f13aeae4fa48b2a1b1068a2a932a
Girish Sivadanam <girish.sivadanam@conweaver.com>
2023-06-04 18:21:11 +0200

    added windows

--- website/src/app/home/home.component.ts
+++ website/src/app/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import {CommonModule} from "@angular/common";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {CdkDrag} from "@angular/cdk/drag-drop";
 
 
 @Component({
   selector: 'home',
   standalone: true,
 
-  imports: [  CommonModule],
+  imports: [  CommonModule, MatCardModule, MatIconModule, CdkDrag],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
