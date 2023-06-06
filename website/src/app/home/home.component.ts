d80a058a9c0f7842412b2fa454bbe2ed8c1dffe1
Girish Sivadanam <girish.sivadanam@conweaver.com>
2023-06-06 23:31:35 +0200

    added information to windows

--- website/src/app/home/home.component.ts
+++ website/src/app/home/home.component.ts
@@ -1,23 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CommonModule} from "@angular/common";
-import {MatCardModule} from "@angular/material/card";
-import {MatIconModule} from "@angular/material/icon";
-import {CdkDrag} from "@angular/cdk/drag-drop";
+import {WincardComponent} from "../wincard/wincard.component";
 
+export interface WorkHistory  {
+  logo: string,
+  isSvg: boolean,
+  duration: string,
+  details: string[],
+  contentIcon?: string,
+  position: string
+}
 
 @Component({
   selector: 'home',
   standalone: true,
-
-  imports: [  CommonModule, MatCardModule, MatIconModule, CdkDrag],
+  imports: [  CommonModule, WincardComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
+  workhistory: WorkHistory[] = [
+    {
+      logo: "conweaver.svg",
+      duration: "nov.19 - present",
+      isSvg: true,
+      details: [
+        "Planning and designing web apps" ,
+        "Implementing tests and improving performance",
+        "Delivering seamless user experiences, driving customer satisfaction"
+      ],
+      contentIcon: 'code-icon',
+      position: "Lead developer"
+    },
+    {
+      logo: "patagona.svg",
+      duration: "Jul.15 - Sept.19",
+      isSvg: false,
+      contentIcon: 'terminal',
+      details: [
+        "Translating ideas into visually appealing websites",
+        "Planning and designing tailored solutions" ,
+        "Converting server-side frontend to Angular"],
+      position: "Web developer"
+    },
+
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
