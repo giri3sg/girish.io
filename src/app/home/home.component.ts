17b7e17b23815fa81c7b85afce1736395d7dd7c0
Girish Sivadanam <girish.sivadanam@conweaver.com>
2023-06-08 22:52:49 +0200

    added minimize

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {WincardComponent} from "../wincard/wincard.component";
+import {CdkDrag} from "@angular/cdk/drag-drop";
 
 export interface WorkHistory  {
   logo: string,
@@ -9,12 +10,13 @@ export interface WorkHistory  {
   details: string[],
   contentIcon?: string,
   position: string
+  skills: {skillName: string, skillLevel: number, color: string}[]
 }
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [  CommonModule, WincardComponent],
+  imports: [  CommonModule, WincardComponent, CdkDrag],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
@@ -25,13 +27,30 @@ export class HomeComponent implements OnInit {
       logo: "conweaver.svg",
       duration: "nov.19 - present",
       isSvg: true,
+      contentIcon: 'code-icon',
+      position: "Lead developer",
       details: [
         "Planning and designing web apps" ,
         "Implementing tests and improving performance",
         "Delivering seamless user experiences, driving customer satisfaction"
       ],
-      contentIcon: 'code-icon',
-      position: "Lead developer"
+      skills: [{
+        color: "blue",
+        skillLevel: 90,
+        skillName: "Angular"
+      },{
+        color: "blue",
+        skillLevel: 80,
+        skillName: "UI/UX"
+      },{
+        color: "blue",
+        skillLevel: 75,
+        skillName: "Azure"
+      },{
+        color: "blue",
+        skillLevel: 99,
+        skillName: "xxx"
+      }]
     },
     {
       logo: "patagona.svg",
@@ -42,7 +61,24 @@ export class HomeComponent implements OnInit {
         "Translating ideas into visually appealing websites",
         "Planning and designing tailored solutions" ,
         "Converting server-side frontend to Angular"],
-      position: "Web developer"
+      position: "Web developer",
+      skills: [{
+        color: "blue",
+        skillLevel: 90,
+        skillName: "Angular"
+      },{
+        color: "blue",
+        skillLevel: 80,
+        skillName: "UI/UX"
+      },{
+        color: "blue",
+        skillLevel: 75,
+        skillName: "Azure"
+      },{
+        color: "blue",
+        skillLevel: 99,
+        skillName: "xxx"
+      }]
     },
 
   ]
